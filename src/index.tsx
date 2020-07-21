de972ac79c7bdcdc9544ace4acf37dcd71ab6627
Delfio <delfio_eu@hotmail.com>
2020-07-20 20:31:30 -0400

    eslint editor config prettier

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,10 @@ import { View, Text } from 'react-native';
 
 // import { Container } from './styles';
 
-const App: React.FC = () => {
-  return (
-      <View>
-          <Text>Bora codaaaaaaaa</Text>
-      </View>
-  );
-}
+const App: React.FC = () => (
+  <View>
+    <Text>Bora codaaaaaaaa</Text>
+  </View>
+);
 
 export default App;
