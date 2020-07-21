d5dd1ef6d50cc070e0428f2837c886fb8e72a8ef
Delfio <delfio_eu@hotmail.com>
2020-07-20 21:31:14 -0400

    use expo google fonts

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,29 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
+
+import { useFonts, RobotoSlab_400Regular, RobotoSlab_500Medium } from '@expo-google-fonts/roboto-slab';
+import { AppLoading } from 'expo';
+
 import Routes from './routes';
 
 // import { Container } from './styles';
 
-const App: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar style="light" backgroundColor="#312e38" />
-    <Routes />
-  </NavigationContainer>
-);
+const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    RobotoSlab_400Regular, RobotoSlab_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <NavigationContainer>
+      <StatusBar style="light" backgroundColor="#312e38" />
+      <Routes />
+    </NavigationContainer>
+  );
+};
 
 export default App;
