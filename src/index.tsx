a7d1dd25a11039e33fb827e51a85ed9daa49da21
Delfio <delfio_eu@hotmail.com>
2020-07-20 21:00:24 -0400

    routes and navigation

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { View, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './routes';
 
 // import { Container } from './styles';
 
 const App: React.FC = () => (
-  <>
+  <NavigationContainer>
     <StatusBar style="light" backgroundColor="#312e38" />
-    <View style={{ backgroundColor: '#312e38', flex: 1 }}>
-      <Text>Bora codaaaaaaaa</Text>
-    </View>
-  </>
+    <Routes />
+  </NavigationContainer>
 );
 
 export default App;
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const SignIn: React.FC = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text style={{ color: '#fff' }}>SignIn</Text>
+  </View>
+);
+
+export default SignIn;
