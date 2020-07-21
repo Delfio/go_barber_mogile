88c41cd400265e87d9880eee793facca72c26b33
Delfio <delfio_eu@hotmail.com>
2020-07-20 20:31:41 -0400

    status bar

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 // import { Container } from './styles';
 
 const App: React.FC = () => (
-  <View>
-    <Text>Bora codaaaaaaaa</Text>
-  </View>
+  <>
+    <StatusBar style="light" backgroundColor="#312e38" />
+    <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+      <Text>Bora codaaaaaaaa</Text>
+    </View>
+  </>
 );
 
 export default App;
