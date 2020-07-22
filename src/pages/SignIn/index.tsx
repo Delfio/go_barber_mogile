ba3dde67d1d5f45d02a147530311d875745ed3c2
Delfio <delfio_eu@hotmail.com>
2020-07-21 21:13:29 -0400

    creation of input and button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, Text } from 'react-native';
 import logoImg from '../../assets/logo.png';
 
+import Input from '../../components/input';
+import Button from '../../components/button';
 import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => (
   <Container>
     <Image source={logoImg} />
     <Title>SignIn</Title>
+
+    <Input name="e-mail" icon="mail" placeholder="E-mail" />
+    <Input name="password" icon="lock" placeholder="Senha" />
+
+    <Button onPress={() => console.log('deu')}>
+      Entrar
+    </Button>
   </Container>
 );
 
