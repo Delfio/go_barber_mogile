85baf11a59e93790b5b3110290210746c096e870
Delfio <delfio_eu@hotmail.com>
2020-07-21 21:51:04 -0400

    Logon Screen

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,23 +1,66 @@
 import React from 'react';
-import { Image, Text } from 'react-native';
+import {
+  Image, View, KeyboardAvoidingView, Platform, ScrollView,
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
 import logoImg from '../../assets/logo.png';
 
 import Input from '../../components/input';
 import Button from '../../components/button';
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPasswordContainer,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountText,
+} from './styles';
 
 const SignIn: React.FC = () => (
-  <Container>
-    <Image source={logoImg} />
-    <Title>SignIn</Title>
+  <>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      style={{ flex: 1 }}
+      enabled
+    >
+      <ScrollView
+        contentContainerStyle={{ flex: 1 }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <Container>
+          <Image source={logoImg} />
 
-    <Input name="e-mail" icon="mail" placeholder="E-mail" />
-    <Input name="password" icon="lock" placeholder="Senha" />
+          <View>
+            <Title>Faça seu Logon</Title>
+          </View>
 
-    <Button onPress={() => console.log('deu')}>
-      Entrar
-    </Button>
-  </Container>
+          <Input name="e-mail" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
+
+          <Button onPress={() => console.log('deu')}>
+            Entrar
+          </Button>
+
+          <ForgotPasswordContainer>
+            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+          </ForgotPasswordContainer>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+
+    {/**
+           * TODO -
+           * Retirar da tela o botão de Criar nova conta quando o teclado é invocado,
+           * pois no android atrapalha
+           *
+           * ANIMAR A RETIRADA
+           */
+        }
+    <CreateAccountButton onPress={() => {}}>
+      <Feather name="log-in" size={20} color="#ff9000" />
+      <CreateAccountText>Criar uma nova conta</CreateAccountText>
+    </CreateAccountButton>
+  </>
 );
 
 export default SignIn;
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,8 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
+import { Platform, Dimensions } from 'react-native';
+
+const { OS } = Platform;
+const { width: ScreenWidth } = Dimensions.get('screen');
 
 export const Container = styled.View`
   flex: 1;
@@ -12,4 +16,43 @@ export const Title = styled.Text`
   color: #f4ede8;
   font-family: 'RobotoSlab_500Medium';
   margin: 64px 0 24px;
+`;
+
+export const ForgotPasswordContainer = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #f4ede8;
+  font-size: 16px;
+  font-family: 'RobotoSlab_400Regular'
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+
+  right: 0;
+  background: #312e38;
+  border-top-width: 1px;
+  border-color: #232129;
+  padding: 16px 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+
+
+  ${OS === 'ios' && Math.round(ScreenWidth) >= 2000 ? css`
+    padding: 16px 0 20px;
+  ` : null}
+`;
+
+export const CreateAccountText = styled.Text`
+  color: #ff9000;
+  font-size: 18px;
+  font-family: 'RobotoSlab_400Regular';
+  margin-left: 16px;
 `;
